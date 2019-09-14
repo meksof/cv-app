48e556c6d754e9e6c71b12609d80f3694d1501c6
Sofiene <meksof@gmail.com>
2019-09-15 00:55:01 +0200

    Fix Production url

--- frontend_src/src/constants.js
+++ frontend_src/src/constants.js
@@ -1,9 +1,10 @@
 const env = process.env.NODE_ENV;
+// const prodUrl = process.env.PRODUCTION_URL; // TODO: will be dynamic from .env file
 const appConstants = {
     env: env,
     WEBSITE_URL :
         env == "development"
         ? "http://localhost:1337"
-        : process.env.PRODUCTION_URL
+        : "https://dev-front.herokuapp.com"
 }
 export default appConstants;
